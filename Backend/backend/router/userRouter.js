72a6652c9e0c634c6b67ec585169309d8b100bc1
srijan-12 <110037710+srijan-12@users.noreply.github.com>
2025-06-13 11:36:11 +0530

    Update userRouter.js

--- Backend/backend/router/userRouter.js
+++ Backend/backend/router/userRouter.js
@@ -15,5 +15,13 @@ userRouter.post('/login', satizeLoginDetails,login)
 userRouter.get('/chat', authCheck)
 userRouter.get("/auth", authCheck,getCurrentuser)
 userRouter.get("/search", authCheck, searchForUser)
+userRouter.post('/api/users/logout', (req, res) => {
+  res.clearCookie('token', {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'None'
+  });
+  res.status(200).json({ message: 'Logged out successfully' });
+});
 
 export default userRouter
