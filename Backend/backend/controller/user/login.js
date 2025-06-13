06e8b43cf310584adca1d2cb03eccb94854f30b6
srijan-12 <110037710+srijan-12@users.noreply.github.com>
2025-06-13 11:20:12 +0530

    Update login.js

--- Backend/backend/controller/user/login.js
+++ Backend/backend/controller/user/login.js
@@ -10,7 +10,7 @@ export default async function login(req,res){
             const passResult = await foundUser.comparePassword(password)
             if(passResult){
                 const token = await foundUser.getJWT()
-                res.cookie("token", token, {maxAge : 1000*60*60*24*7})
+                res.cookie('token', token, {httpOnly: true,secure: true,sameSite: 'None',maxAge: 7 * 24 * 60 * 60 * 1000});
                 return res.status(200).json({result: "user logged in", error : null})
             }else{
                 throw new Error("in-valid credentials")
